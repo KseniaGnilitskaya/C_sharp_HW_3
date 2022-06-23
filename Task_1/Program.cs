c4ce76062ff306a1e9b82f4595c7f7a37739a056
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-23 23:41:05 +0300

    Task 1 added

---
+++ Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// FIRST SOLUTION
+
+/* Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <=number){
+    Console.Write("Cubes table: ");
+    Console.WriteLine(Math.Pow(i, 3));
+    i+=1;
+} */
+
+
+// SECOND SOLUTION
+
+//Console.Write("Enter the number: ");
+//int number = int.Parse(Console.ReadLine());
+//Console.Write("Cubes table: ");
+//
+//for (int i=1, i<number, i++){
+//    Console.Write(Math.Pow(i, 3));
+//}
+
+
+//THIRD SOLUTION
+
+Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+
+
+
