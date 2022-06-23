a5fa3fe1ae7b142a8aadfe05d7489bd35727dcb0
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-24 00:16:10 +0300

    Task 1 added

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,28 +5,19 @@ int number = int.Parse(Console.ReadLine());
 int i = 1;
 
 while (i <=number){
-    Console.Write("Cubes table: ");
     Console.WriteLine(Math.Pow(i, 3));
     i+=1;
 } */
 
 
-// SECOND SOLUTION
-
-//Console.Write("Enter the number: ");
-//int number = int.Parse(Console.ReadLine());
-//Console.Write("Cubes table: ");
-//
-//for (int i=1, i<number, i++){
-//    Console.Write(Math.Pow(i, 3));
-//}
-
-
-//THIRD SOLUTION
+// SECOND SOLUTION    
 
 Console.Write("Enter the number: ");
 int number = int.Parse(Console.ReadLine());
+Console.Write("Cubes table: ");
 
+for (int i = 1; i <= number; i++){
+    Console.WriteLine(Math.Pow(i, 3));
+}
 
-
-
+// Я не стала решать задачу с массивами, так как мне кажется, они тут совсем не нужны, это не тот случай
