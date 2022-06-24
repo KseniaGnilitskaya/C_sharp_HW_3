9f65ae731c89bd1278ba9cb32d99ab0bbeb51e56
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-24 16:47:55 +0300

    Task 3 updated

---
+++ Task_3_palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter 5-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 9999 & number < 100000){
+    string str = number.ToString();
+    if (str[0] == str[4] & str[1] == str[3]){
+        Console.Write("Yes, it's a palindrome");
+    }
+    else{
+        Console.Write("No, it's not a palindrome");
+    }
+}
+else{
+    Console.Write("Your number is not correct. Try again.");
+} 
+
+
+
+
+
+
+
+
+
+
